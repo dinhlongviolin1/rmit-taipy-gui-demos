30dfb79a3cab697908ed06b92e32334c04207294
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-12-19 01:34:05 +0700

    first commit

---
+++ demo_compound_interest.py
@@ -0,0 +1,48 @@
+from taipy.gui import Gui
+import pandas as pd
+
+interest_rate = 8
+contribution_increase_rate = 10
+saving_term = 5
+deposit_amount = 0
+monthly_contribution = 5000
+
+data = pd.DataFrame(columns=["Year", "Contribution", "Interest", "Total"])
+
+def calculate(state):
+    calculate_data = []
+    total_interest = 0
+    total = state.deposit_amount
+    contribution = state.monthly_contribution
+    for year in range(int(state.saving_term)):
+        interest_rate_per_month = state.interest_rate / 100 / 12
+        for _ in range(12):
+            total = total * (interest_rate_per_month + 1) + contribution
+            total_interest += total * interest_rate_per_month
+        contribution = contribution * (state.contribution_increase_rate / 100 + 1)
+        calculate_data.append((year + 1, total-total_interest, total_interest, total))
+    state.data = pd.DataFrame(calculate_data, columns=["Year", "Contribution", "Interest", "Total"])
+
+page = """
+# Compound interest rate calculator (compounded monthly)
+
+<|layout|columns=6*1|
+
+<|{deposit_amount}|number|label=Initial Deposit Amount (USD)|>
+
+<|{monthly_contribution}|number|label=Monthly Contribution (USD)|>
+
+<|{contribution_increase_rate}|number|label=Yearly Contribution Increase Rate (%)|>
+
+<|{interest_rate}|number|label=Interest rate per year (%)|>
+
+<|{saving_term}|number|label=Saving term (year)|>
+
+<|Calculate|button|on_action=calculate|>
+
+|>
+
+<|{data}|chart|type=bar|x=Year|y[1]=Interest|y[2]=Contribution|y[3]=Total|height=600px|>
+"""
+
+Gui(page=page).run()
