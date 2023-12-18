0e5f3e0710e05ef3c42851a2b4aaf12ad53e564c
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-12-19 02:12:24 +0700

    update code structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Demos for Taipy GUI
+
+A collective of Taipy GUI demos
+
+## Installing dependencies
+```bash
+# Install using pip
+pip install -r requirements.txt
+
+# Install using pipenv
+pipenv install
+```
--- demo_compound_interest.py
+++ demo_compound_interest.py
@@ -1,27 +1,36 @@
 from taipy.gui import Gui
 import pandas as pd
 
+# Initialize variables
 interest_rate = 8
 contribution_increase_rate = 10
 saving_term = 5
 deposit_amount = 0
 monthly_contribution = 5000
 
-data = pd.DataFrame(columns=["Year", "Contribution", "Interest", "Total"])
+# Setting up a variable that will be used for populating the chart
+chart_data = pd.DataFrame(columns=["Year", "Contribution", "Interest", "Total"])
 
+
+# This function will be executed when the user clicks the "Calculate" button
 def calculate(state):
     calculate_data = []
     total_interest = 0
     total = state.deposit_amount
     contribution = state.monthly_contribution
+    # Calculate compound interest
     for year in range(int(state.saving_term)):
         interest_rate_per_month = state.interest_rate / 100 / 12
         for _ in range(12):
             total = total * (interest_rate_per_month + 1) + contribution
             total_interest += total * interest_rate_per_month
         contribution = contribution * (state.contribution_increase_rate / 100 + 1)
-        calculate_data.append((year + 1, total-total_interest, total_interest, total))
-    state.data = pd.DataFrame(calculate_data, columns=["Year", "Contribution", "Interest", "Total"])
+        calculate_data.append((year + 1, total - total_interest, total_interest, total))
+    # Update the chart through the state variable
+    state.chart_data = pd.DataFrame(
+        calculate_data, columns=["Year", "Contribution", "Interest", "Total"]
+    )
+
 
 page = """
 # Compound interest rate calculator (compounded monthly)
@@ -42,7 +51,7 @@ page = """
 
 |>
 
-<|{data}|chart|type=bar|x=Year|y[1]=Interest|y[2]=Contribution|y[3]=Total|height=600px|>
+<|{chart_data}|chart|type=bar|x=Year|y[1]=Interest|y[2]=Contribution|y[3]=Total|height=600px|>
 """
 
 Gui(page=page).run()
